916692d6351a5d62eff77e43a28e624a0a9ee24e
PDileepkumar <dileepkumar96.p@gmail.com>
2018-02-28 23:14:04 +0530

    nabh_designers

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+$('#myCarousel').carousel({
+	interval:3000;
+	pause:null;
+})
